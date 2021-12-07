43d1f7d05c309b7b84f74d5453979df9e3130bea
audemar <audemarsadish@gmail.com>
2021-12-07 15:23:58 +0100

    exo6 terminé

---
+++ exo6.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="index.css"/>
+    <title>PARTIE 1 EXO 6</title>
+</head>
+<body>
+    <p>
+   <?php
+   $name = 'Sadish';
+   echo 'Bonjour'.' '.$name.', comment vas-tu?' ;
+   ?>
+   </p>
+
+</body>
+</html>
